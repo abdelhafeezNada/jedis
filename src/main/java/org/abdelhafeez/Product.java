7ecf0252a3ffdad1d15a467b9f419ac4f9c56f65
AbdelhafeezNada <abdelhafeez0nada@gmail.com>
2024-05-20 10:05:10 +0300

    create product entity

---
+++ src/main/java/org/abdelhafeez/Product.java
@@ -0,0 +1,20 @@
+package org.abdelhafeez;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    private Long id;
+
+    private String name;
+
+    private double price;
+
+}
